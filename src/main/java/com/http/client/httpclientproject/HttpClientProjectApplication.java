7b3d8e342e5b4085f71a68454c7740c36b1ed5ba
ekal0323 <ekal0323@iabacus.co.kr>
2024-05-09 10:28:57 +0900

    first commit

---
+++ src/main/java/com/http/client/httpclientproject/HttpClientProjectApplication.java
@@ -0,0 +1,13 @@
+package com.http.client.httpclientproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpClientProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/http/client/httpclientproject/HttpClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http.client.httpclientproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
